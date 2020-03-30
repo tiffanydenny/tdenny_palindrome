6f172150bf577202da4d66147f10a203204ec5b2
Tiffany Denny <tiffany@tiffanydenny.com>
2020-03-30 12:11:28 -0500

    Initialize repository

---
+++ lib/tdenny_palindrome.rb
@@ -0,0 +1,6 @@
+require "tdenny_palindrome/version"
+
+module TdennyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tdenny_palindrome/version.rb
@@ -0,0 +1,3 @@
+module TdennyPalindrome
+  VERSION = "0.1.0"
+end
